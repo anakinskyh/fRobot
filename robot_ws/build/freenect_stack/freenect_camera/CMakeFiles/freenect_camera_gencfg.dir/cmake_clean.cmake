f12526628ebd1168b6b2fd643a7cbf9e8f1d56f3
anakinskyh <anakin.sky@protonmail.com>
2017-02-04 21:28:06 +0700

    add camera

---
+++ robot_ws/build/freenect_stack/freenect_camera/CMakeFiles/freenect_camera_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/freenect_camera_gencfg"
+  "/home/anakin/Workspace/2016/Robot/fRobot/robot_ws/devel/include/freenect_camera/FreenectConfig.h"
+  "/home/anakin/Workspace/2016/Robot/fRobot/robot_ws/devel/share/freenect_camera/docs/FreenectConfig.dox"
+  "/home/anakin/Workspace/2016/Robot/fRobot/robot_ws/devel/share/freenect_camera/docs/FreenectConfig-usage.dox"
+  "/home/anakin/Workspace/2016/Robot/fRobot/robot_ws/devel/lib/python2.7/dist-packages/freenect_camera/cfg/FreenectConfig.py"
+  "/home/anakin/Workspace/2016/Robot/fRobot/robot_ws/devel/share/freenect_camera/docs/FreenectConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/freenect_camera_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ robot_ws/build/freenect_stack/freenect_stack/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/anakin/Workspace/2016/Robot/fRobot/robot_ws/src/freenect_stack/freenect_stack
+# Build directory: /home/anakin/Workspace/2016/Robot/fRobot/robot_ws/build/freenect_stack/freenect_stack
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
